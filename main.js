2c832e075f952c1dcc3c83b369ce6ab18859f0c6
msa01 <markstevenallen@me.com>
2016-09-29 16:52:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox.png') {
-      myImage.setAttribute ('src','images/Apps-firefox-icon.png');
+    if(mySrc === 'firefox.png') {
+      myImage.setAttribute ('src','Apps-firefox-icon.png');
     } else {
-      myImage.setAttribute ('src','images/firefox.png');
+      myImage.setAttribute ('src','firefox.png');
     }
 }
 var mybutton = document.querySelector('button');
